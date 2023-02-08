5391fdda3a70c7dd44451b5f7417140dde308e29
issach315 <issachemani227@gmail.com>
2023-02-08 14:42:24 +0530

    conditionl operator

--- Day1/Third.js
+++ Day1/Third.js
@@ -1,8 +1,8 @@
-let x=10;
-    // reinitialization is possible
-    x=12;
-    console.log(x);
-const y=9;
-    // reinitialization is  Not possible
-    //y=15
-    console.log(y);
+
+
+
+
+    let n=12;
+    let n1=10;
+
+    n >= n1?console.log('Issach'):console.log('Emani');
